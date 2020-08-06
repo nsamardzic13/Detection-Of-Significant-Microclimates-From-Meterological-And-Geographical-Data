859a86f74f8f84643a1b1ee064f1fccf9499c38c
nsamardzic13 <nsamardzic@riteh.hr>
2020-08-07 00:16:40 +0200

    Specific days for autocorelation

--- detection_main.py
+++ detection_main.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 
 # define global variables
 autocorrelation_text = ' these towns have microclimates based on autocorrelation throughout day: '
-autocorrelation_difference = 0.10   # x% differene
+autocorrelation_weather_condition = 'vedro'
+autocorrelation_difference = 0.20   # x% differene
 def_height = 100    # max height difference
 def_dist = 50       # max distance difference
-def_cnt = 3         # number of towns around
+def_cnt = 10         # number of towns around
 
 # get data from csv files
 def import_csv_file():
@@ -24,6 +25,12 @@ def list_title(list):
     ret_list = []
     for row in list:
         ret_list.append(row.title())
+    return ret_list
+
+# get top n days with desired weather
+def get_dates(data, value):
+    tmp_ret_list = data.loc[data['Weather'] == value][['Date']]
+    ret_list = tmp_ret_list['Date'].value_counts().nlargest(7).index.tolist()
     return ret_list
 
 # get autocorrelation
@@ -49,7 +56,7 @@ def daily_temp(data, filter_dates, names, res_dict):
             if name in res_dict[date]:
                 plt.plot(x_values, y_values, alpha=0.6, linewidth=4)
             else:
-                plt.plot(x_values, y_values, alpha=0.4)
+                plt.plot(x_values, y_values, alpha=0.4, ls='--')
 
         # add title, legend and gird to every plot
         plt.title('Temperature for ' + str(date))
@@ -82,8 +89,9 @@ def check_microclimates(data, filter_dates, names, allowed_difference , distance
                 towns_distance = distance.loc[(distance['Town1'] == name) & (distance['Town2'] == name2)]['Distance'].item()
                 town_altitude = altitude.loc[(altitude['Town'] == name2)]['Elevation'].item()
                 if abs_diff > allowed_difference:
-                    if towns_distance < diff_dist and np.abs(altitude_value - town_altitude) < diff_height:
-                        tmp_cnt = tmp_cnt + 1
+                    # if towns_distance < diff_dist and np.abs(altitude_value - town_altitude) < diff_height:
+                    #     tmp_cnt = tmp_cnt + 1
+                    tmp_cnt = tmp_cnt + 1
             if tmp_cnt > default_cnt:
                 tmp_list.append(name)
         if tmp_list:
@@ -104,17 +112,18 @@ def main():
     unique_towns = list(df_altitude.sort_values(by='Town')['Town'])     # get unique names of towns ordered by name
     filter_dates = list(df_data_pgz.ffill()['Date'].unique())  # list of dates we want to filer, after change od 23:30 - try removing [1:]
 
-    # # call functions to get microclimates values based on autocorrelation
-    # autocorrelation = get_autocorrelation(data=df_data_pgz, filter_dates=filter_dates, names=unique_towns, autocorr_column='Temperature') # call function to get nested dict of autocorrelation for all places by date
-    # microclimates_autocorrelation = check_microclimates(data=autocorrelation, filter_dates=filter_dates, names=unique_towns,
-    #                                                     allowed_difference=autocorrelation_difference, distance=df_distance, altitude=df_altitude) #check if we have microclimates
-    # # output of microclimates based on autocorrelation
-    # if microclimates_autocorrelation:
-    #     dates_microclimates = list(microclimates_autocorrelation.keys())    #get dates when we have towns with microclimates
-    #     print_towns(microclimates_autocorrelation, autocorrelation_text)    #print towns and dates with microclimates
-    #     daily_temp(data=df_data_pgz, filter_dates=dates_microclimates, names=unique_towns, res_dict=microclimates_autocorrelation)  # call function to plot temperature by date for all places
-    # else:
-    #     print('There are no towns with microclimates with current variables')
+    # call functions to get microclimates values based on autocorrelation
+    autocorrelation_days = get_dates(df_data_pgz, autocorrelation_weather_condition)
+    autocorrelation = get_autocorrelation(data=df_data_pgz, filter_dates=autocorrelation_days, names=unique_towns, autocorr_column='Temperature') # call function to get nested dict of autocorrelation for all places by date
+    microclimates_autocorrelation = check_microclimates(data=autocorrelation, filter_dates=autocorrelation_days, names=unique_towns,
+                                                        allowed_difference=autocorrelation_difference, distance=df_distance, altitude=df_altitude) #check if we have microclimates
+    # output of microclimates based on autocorrelation
+    if microclimates_autocorrelation:
+        dates_microclimates = list(microclimates_autocorrelation.keys())    #get dates when we have towns with microclimates
+        print_towns(microclimates_autocorrelation, autocorrelation_text)    #print towns and dates with microclimates
+        daily_temp(data=df_data_pgz, filter_dates=dates_microclimates, names=unique_towns, res_dict=microclimates_autocorrelation)  # call function to plot temperature by date for all places
+    else:
+        print('There are no towns with microclimates with current variables')
 
     # build 2d array having shape (filter_dates, unique_towns) and average temperatures
     df_svd = df_data_pgz.loc[:, ['Town', 'Date', 'Temperature']].groupby(['Town', 'Date'])['Temperature'].mean()
